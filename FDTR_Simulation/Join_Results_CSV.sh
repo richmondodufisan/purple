7ee6772030c068446e204a3e3bd574f9c0c5e3c6
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2023-11-01 02:56:15 -0500

    last edit, still with errors

--- FDTR_Simulation/Join_Results_CSV.sh
+++ FDTR_Simulation/Join_Results_CSV.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+n_iterations=2
+
+theta_angle=0
+n_periods=1
+stop_line_number=$(python3 -c "import math; print((396*$n_periods)+1)")
+
+echo $stop_line_number
+
 # Output file
-output_file="MOOSE_theta_75_4periods.csv"
+output_file="../MOOSE_theta_${theta_angle}_T2.csv"
 
 # Create header for the output file
 echo "freq, x0, time, delta_temp" > "$output_file"
@@ -9,18 +17,31 @@ echo "freq, x0, time, delta_temp" > "$output_file"
 # Get a list of files that fit the pattern using ls -d
 # file_list=$(ls -d fdtr_input_freq_*_x0_*.csv)
 
-x0_vals_num=("-15" "-10" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "10" "15")
-freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
-
-#x0_vals_num=("-15" "-10" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "10" "15")
+#x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
 #freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
+
+x0_vals_num=("0")
+freq_vals_num=("1e6")
+
+
+
 
 for x0 in "${x0_vals_num[@]}"; do
 	for freq in "${freq_vals_num[@]}"; do
-		input_file="FDTR_input_theta_0_freq_${freq}_x0_${x0}_out.csv"
-		
-		# Concatenate data to the output file using printf in awk
-		awk -v freq="$freq" -v x0="$x0" -F, 'NR>1{printf "%s, %s, %.20f, %.20f\n", x0 , freq / 1e6, $1 * 1e6, $2}' "$input_file" >> "$output_file"
+	
+		current_iteration=1
+		while [ $current_iteration -le $n_iterations ]; do
+			input_file="FDTR_input_theta_${theta_angle}_freq_${freq}_x0_${x0}_v${current_iteration}_out.csv"
+			
+			# Concatenate data to the output file using printf in awk, stopping at the specified line
+			awk -v freq="$freq" -v x0="$x0" -v stop_line="$stop_line_number" -F, 'NR>2{
+				if (NR <= stop_line) {
+					printf "%s, %s, %.20f, %.20f\n", x0, freq / 1e6, $1 * 1e6, $3
+				}
+			}' "$input_file" >> "$output_file"
+			
+			current_iteration=$((current_iteration + 1))
+		done
 	done
 done
 
--- FDTR_Simulation/Parameter_Sweep_Local.sh
+++ FDTR_Simulation/Parameter_Sweep_Local.sh
@@ -1,114 +1,65 @@
 #!/bin/bash
 
+# Set the maximum number of times to submit the batch job
+n_iterations=2
+
+# Set the number of periods each job/sweep should solve for
+n_periods_per_job=1.0
+
+# Initial timestep
+start_val=0.0
+
+# Submit the initial parameter sweep
+
 #Original file name
 og_filename="FDTR_input"
 extension=".i"
 
 og_mesh_script="FDTR_mesh"
-og_mesh_ext=".geo"
+og_mesh_ext=".py"
 
+first_period=$n_periods_per_job
 
 # Define the range of values you want to loop over
 
-x0_vals_num=("-15" "0")
-
-#freq_vals_num=("1e6")
-
-theta_vals_num=("75" "0")
-
-#x0_vals_num=("-15" "-10" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "10" "15")
+x0_vals_num=("0")
 
 freq_vals_num=("1e6")
 
-#theta_vals_num=("0" "15" "30" "45" "60" "75")
+theta_vals_num=("0")
+
+# x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
+
+# freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
+
+# theta_vals_num=("0")
 
 
-
-# Loop over each x0_val value
-for theta_val_num in "${theta_vals_num[@]}"; do
-	for x0_val_num in "${x0_vals_num[@]}"; do
+# Loop over values
+for x0_val_num in "${x0_vals_num[@]}"; do
+	for theta_val_num in "${theta_vals_num[@]}"; do
 	
 		# Replace the x0_val value in the mesh script
 		sed -i "s/\(xcen\s*=\s*\)[0-9.eE+-]\+/\1$x0_val_num/g" "${og_mesh_script}${og_mesh_ext}"
 		
-		theta_rad=$(echo "scale=10; (90.0 - $theta_val_num) / 180.0 * 3.14159265359" | bc -l)
-		tan_theta=$(python3 -c "import math; print(-1.0*math.tan($theta_rad))")
-		#cos_theta=$(python3 -c "import math; print(math.cos($theta_rad))")
-		cos_theta=1
+		# Replace the theta_val value in the mesh script
+		sed -i "0,/theta\s*=\s*[0-9.eE+-]\+/{s//theta = $theta_val_num/}" "${og_mesh_script}${og_mesh_ext}"
 		
-		# The following are for adjusting the grain boundary refinement in the mesh script
-		# These "corrections" ensure that the boundaries align and thus keep the mesh conforming
-		# It is done using simple theory of the equation of a line
-		xlen=40.0
-		zlen=40.0
-		gb_width=0.1
+		# Replace the mesh name in the mesh script
+		new_mesh_name="${og_mesh_script}_x0_${x0_val_num}_theta_${theta_val_num}.msh"
+		sed -i "0,/newMeshName = [^ ]*/s/newMeshName = [^ ]*/newMeshName = \"$new_mesh_name\"/" "${og_mesh_script}${og_mesh_ext}"	
 		
-		# refine region 10x the size of the grain boundary to the left and right
-		part_width=$(echo "scale=10; ($gb_width/$cos_theta)*10.0" | bc -l)
-		
-		xleft_up_val=$(echo "scale=10; -($part_width/2.0)" | bc -l)
-		xright_up_val=$(echo "scale=10; ($part_width/2.0)" | bc -l)
-		
-		# Replace value(s) in the mesh script
-		sed -i "s/\(x_left_up\s*=\s*\)[0-9.eE+-]\+/\1$xleft_up_val/g" "${og_mesh_script}${og_mesh_ext}"			
-		sed -i "s/\(x_right_up\s*=\s*\)[0-9.eE+-]\+/\1$xright_up_val/g" "${og_mesh_script}${og_mesh_ext}"
-		
-		######### Getting x & z co-ordinates for LEFT side #########
-		xleft_down_val=$(echo "scale=10; (1.0/$tan_theta)*(-$zlen+$tan_theta*$xleft_up_val)" | bc -l)
-		zleft_down_val=-$zlen
-		
-		# Editing angle in mesh, see mesh .geo file for more details
-		if [ "$(echo "$xleft_down_val >= $xlen" | bc -l)" -eq 1 ]; then
-			xleft_down_val=$xlen
-			zleft_down_val=$(echo "($tan_theta*$xlen)-($tan_theta*$xleft_up_val)" | bc -l)
-		fi
-
-		# Checking for zero angle
-		is_angle_zero=$(python -c "theta_val_num = $theta_val_num; result = 1 if -1e-8 <= theta_val_num <= 1e-8 else 0; print(result)")
-
-		if [ "$is_angle_zero" -eq 1 ]; then
-			xleft_down_val=$xleft_up_val
-			zleft_down_val=-$zlen
-		fi
-		
-		# Replace value(s) in the mesh script
-		sed -i "s/\(x_left_down\s*=\s*\)[0-9.eE+-]\+/\1$xleft_down_val/g" "${og_mesh_script}${og_mesh_ext}"			
-		sed -i "s/\(z_left_down\s*=\s*\)[0-9.eE+-]\+/\1$zleft_down_val/g" "${og_mesh_script}${og_mesh_ext}"
-		
-		
-		
-		######### Getting x & z co-ordinates for RIGHT side #########
-		xright_down_val=$(echo "scale=10; (1.0/$tan_theta)*(-$zlen+$tan_theta*$xright_up_val)" | bc -l)
-		zright_down_val=-$zlen
-		
-		# Editing angle in mesh, see mesh .geo file for more details
-		if [ "$(echo "$xright_down_val >= $xlen" | bc -l)" -eq 1 ]; then
-			xright_down_val=$xlen
-			zright_down_val=$(echo "($tan_theta*$xlen)-($tan_theta*$xright_up_val)" | bc -l)
-		fi
-		
-		if [ "$is_angle_zero" -eq 1 ]; then
-			xright_down_val=$xright_up_val
-			zright_down_val=-$zlen
-		fi
-		
-		
-		# Replace value(s) in the mesh script
-		sed -i "s/\(x_right_down\s*=\s*\)[0-9.eE+-]\+/\1$xright_down_val/g" "${og_mesh_script}${og_mesh_ext}"			
-		sed -i "s/\(z_right_down\s*=\s*\)[0-9.eE+-]\+/\1$zright_down_val/g" "${og_mesh_script}${og_mesh_ext}"
-		
-			
+		#echo "$new_mesh_name"
 		
 		# Make new 3D mesh
-		new_mesh_name="${og_mesh_script}_theta_${theta_val_num}_x0_${x0_val_num}.msh"
-		gmsh "${og_mesh_script}${og_mesh_ext}" -3 -o "$new_mesh_name" -save_all >> gmsh_output.txt 2>&1 &
-		wait
+		#python3 FDTR_mesh.py >> gmsh_output.txt &
+		#wait
 		
 		echo "Mesh Generated, x0 = ${x0_val_num}, theta = ${theta_val_num}"
-	
+		
 		for freq_val_num in "${freq_vals_num[@]}"; do
 			# Create a new filename by appending x0_val to the original filename
-			new_filename="${og_filename}_theta_${theta_val_num}_freq_${freq_val_num}_x0_${x0_val_num}.i"
+			new_filename="${og_filename}_theta_${theta_val_num}_freq_${freq_val_num}_x0_${x0_val_num}_v1.i"
 
 			# Copy the original input file to the new filename
 			cp "$og_filename$extension" "$new_filename"
@@ -124,16 +75,84 @@ for theta_val_num in "${theta_vals_num[@]}"; do
 			
 			# Replace the mesh in the MOOSE script
 			sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$new_mesh_name\"/" "$new_filename"
-
-			# Start simulation and wait for it to finish
-			#../purple-opt -i ${new_filename} --mesh-only &
-
-			# ../purple-opt -i ${new_filename} &
-			wait
+			
+			# Replace the end period
+			sed -i "s/\(end_period\s*=\s*\)[0-9.eE+-]\+/\1$first_period/g" "$new_filename"
+			
+			# mpiexec -n 4 ../purple-opt -i ${new_filename} &
+			# wait
 		done
 	done
 done
 
-# Delete input files and mesh files
-# rm ${og_filename}_*.i
-# rm ${og_mesh_script}_*.msh
+submission_count=1
+o_start=$start_val
+
+#Initial input file name
+init_filename="FDTR_input"
+
+#Original file name
+og_filename="FDTR_input_restart"
+extension=".i"
+
+# RESTART Loop
+while [ $submission_count -lt $n_iterations ]; do
+    o_ver=$(echo "$submission_count" | bc -l)
+	n_ver=$(echo "$submission_count + 1" | bc -l)
+	former_sim_ver="v${o_ver}"
+	new_sim_ver="v${n_ver}"
+
+	o_stop=$(echo "$o_start + $n_periods_per_job" | bc -l)
+	n_stop=$(echo "$o_stop + $n_periods_per_job" | bc -l)
+	
+	old_start=$o_start
+	old_end=$o_stop
+	new_start=$o_stop
+	new_end=$n_stop
+
+
+
+	# Loop over values
+	for x0_val_num in "${x0_vals_num[@]}"; do
+		for theta_val_num in "${theta_vals_num[@]}"; do
+			for freq_val_num in "${freq_vals_num[@]}"; do
+			
+				# Create a new filename by appending x0_val to the original filename
+				new_filename="${init_filename}_theta_${theta_val_num}_freq_${freq_val_num}_x0_${x0_val_num}_${new_sim_ver}.i"
+
+				# Copy the original input file to the new filename
+				cp "$og_filename$extension" "$new_filename"
+				
+				# Replace the theta_val value in the new input file
+				sed -i "s/\(theta_deg\s*=\s*\)[0-9.eE+-]\+/\1$theta_val_num/g" "$new_filename"
+				
+				# Replace the freq_val value in the new input file
+				sed -i "s/\(freq_val\s*=\s*\)[0-9.eE+-]\+/\1$freq_val_num/g" "$new_filename"
+
+				# Replace the x0_val value in the new input file
+				sed -i "s/\(x0_val\s*=\s*\)[0-9.eE+-]\+/\1$x0_val_num/g" "$new_filename"
+				
+				# Replace the mesh in the MOOSE script
+				former_sim_output="${init_filename}_theta_${theta_val_num}_freq_${freq_val_num}_x0_${x0_val_num}_${former_sim_ver}_out.e"
+				sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$former_sim_output\"/" "$new_filename"
+				
+				
+				############# Replacing end and start periods #############
+				
+				sed -i "s/\(prev_start\s*=\s*\)[0-9.eE+-]\+/\1$old_start/g" "$new_filename"
+				sed -i "s/\(prev_end\s*=\s*\)[0-9.eE+-]\+/\1$old_end/g" "$new_filename"
+				sed -i "s/\(start_period\s*=\s*\)[0-9.eE+-]\+/\1$new_start/g" "$new_filename"
+				sed -i "s/\(end_period\s*=\s*\)[0-9.eE+-]\+/\1$new_end/g" "$new_filename"
+				
+				############# END Replacing end and start periods #############
+				
+				mpiexec -n 4 ../purple-opt -i ${new_filename} &
+				wait		
+			done
+		done
+	done
+	
+	
+	submission_count=$((submission_count + 1))
+	o_start=$o_stop
+done
