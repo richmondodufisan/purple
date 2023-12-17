28e2fc0b6e768289c8487e0ba0f8107cc30a4abc
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2023-12-17 03:14:23 -0600

    back to coarse mesh, testing thermal conductivity influence on phase

--- FDTR_Simulation_Fourier/3D/Standard_Formulation/Parameter_Sweep_HPC.sh
+++ FDTR_Simulation_Fourier/3D/Standard_Formulation/Parameter_Sweep_HPC.sh
@@ -21,10 +21,10 @@ function check_squeue() {
 
 # Set the maximum number of times to submit the batch job
 #n_iterations=60
-n_iterations=35
+n_iterations=1
 
 # Set the number of periods each job/sweep should solve for
-n_periods_per_job=2.0
+n_periods_per_job=200.0
 
 # Initial timestep
 start_val=0.0
@@ -72,8 +72,8 @@ for x0_val_num in "${x0_vals_num[@]}"; do
 		#echo "$new_mesh_name"
 		
 		# Make new 3D mesh
-		# python3 FDTR_mesh.py >> gmsh_output.txt &
-		# wait
+		python3 FDTR_mesh.py >> gmsh_output.txt &
+		wait
 		
 		# Submit Job
 		#sbatch --wait FDTR_Batch_gmsh.sh
@@ -111,7 +111,7 @@ for x0_val_num in "${x0_vals_num[@]}"; do
 			sed -E -i "s/(#SBATCH --job-name=)[^[:space:]]+/\1${x0_val_num}${freq_noexp}${theta_val_num}_Fourier_Standard_Formulation/" "FDTR_Batch_MOOSE.sh"
 
 			# Submit job
-			# sbatch FDTR_Batch_MOOSE.sh
+			sbatch FDTR_Batch_MOOSE.sh
 		done
 	done
 done
@@ -139,9 +139,9 @@ while [ $submission_count -lt $n_iterations ]; do
 		echo "No jobs in the queue. Submitting batch job script..."
 		
 		# Check for failed jobs. edit date/time/number of failed jobs as needed
-		failed_jobs=$(sacct -S 12/16/23 -X -u vtw1026 --format=nodelist,state,jobid | grep FAIL | wc -l)
+		failed_jobs=$(sacct -S 12/17/23 -X -u vtw1026 --format=nodelist,state,jobid | grep FAIL | wc -l)
 		
-		if [ $failed_jobs -gt 3 ]; then
+		if [ $failed_jobs -gt 0 ]; then
 			echo "SOME JOBS FAILED. EXITING SCRIPT."
 			exit 1
 		fi
