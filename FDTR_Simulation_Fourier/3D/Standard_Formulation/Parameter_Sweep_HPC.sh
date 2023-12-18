c5a3c0dd4fe1a91e4989da8c500f5ed7366ecbc2
Alesanmi Richmond Odufisan <vtw1026@quser31.quest.it.northwestern.edu>
2023-12-17 20:56:31 -0600

    last simulations

--- FDTR_Simulation_Fourier/3D/Standard_Formulation/Parameter_Sweep_HPC.sh
+++ FDTR_Simulation_Fourier/3D/Standard_Formulation/Parameter_Sweep_HPC.sh
@@ -21,10 +21,10 @@ function check_squeue() {
 
 # Set the maximum number of times to submit the batch job
 #n_iterations=60
-n_iterations=1
+n_iterations=40
 
 # Set the number of periods each job/sweep should solve for
-n_periods_per_job=200.0
+n_periods_per_job=2.0
 
 # Initial timestep
 start_val=0.0
