c3c553c4b3b2c906888ba83d363397d4e74d0588
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-01-05 18:02:51 -0600

    calibration simulations

--- FDTR_Simulation_Fourier/3D/Standard_Formulation/Join_Results_CSV.sh
+++ FDTR_Simulation_Fourier/3D/Standard_Formulation/Join_Results_CSV.sh
@@ -3,25 +3,28 @@
 n_iterations=1
 theta_angle=0
 
-# Output file
-output_file="../Fourier_Standard_Med_Coarse_CALIBRATE.csv"
+# Input file type
+# og_filename = FDTR_input
+og_filename = FDTR_CALIBRATION
 
-# Create header for the output file
-echo "x0, freq, time, delta_temp" > "$output_file"
-
-#x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
-#freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
+# x0_vals_num=("-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15")
+# freq_vals_num=("1e6" "2e6" "4e6" "6e6" "10e6")
 
 x0_vals_num=("0")
 freq_vals_num=("1e6" "2e6" "4e6" "6e6" "8e6" "10e6")
 
+# Output file
+output_file="../${og_filename}_out_theta_${theta_angle}.csv"
+
+# Create header for the output file
+echo "x0, freq, time, delta_temp" > "$output_file"
 
 for x0 in "${x0_vals_num[@]}"; do
 	for freq in "${freq_vals_num[@]}"; do
 	
 		current_iteration=1
 		while [ $current_iteration -le $n_iterations ]; do
-			input_file="FDTR_CALIBRATION_Fourier_Standard_theta_${theta_angle}_freq_${freq}_x0_${x0}_v${current_iteration}_out.csv"
+			input_file="${og_filename}_Fourier_Standard_theta_${theta_angle}_freq_${freq}_x0_${x0}_v${current_iteration}_out.csv"
 			
 			# Concatenate data to the output file using printf in awk, stopping at the specified line
 			awk -v freq="$freq" -v x0="$x0" -F, 'NR>2{
