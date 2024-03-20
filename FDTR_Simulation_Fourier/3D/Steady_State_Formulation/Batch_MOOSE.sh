b90c4d6478d5af7425fe397312f8e12072e19c81
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-03-20 12:01:17 -0500

    added single layer test for debugging

--- FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Batch_MOOSE.sh
+++ FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Batch_MOOSE.sh
@@ -4,12 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=25 ## how many cpus or processors do you need on each computer
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem-per-cpu=2G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=steady_fourier  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --exclude=qnode0565,qnode0626,qnode0637,qnode0019
 
 
-script_name="FDTR_input.i"
+script_name="SingleLayer_Test.i"
 
 #moose_exec.sh ../purple-opt -i ${script_name} --mesh-only
 #moose_exec.sh ../purple-opt -i ${script_name}
