479c0d48586a1f63b2c6b43f5669ac7036562976
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-03-24 03:29:02 -0500

    calibration simulations

--- FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Join_Results_CSV.sh
+++ FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Join_Results_CSV.sh
@@ -3,14 +3,14 @@
 theta_angle=0
 
 # Input file type
-og_filename=FDTR_input_GibbsExcess
-# og_filename=FDTR_CALIBRATION
+# og_filename=FDTR_input_GibbsExcess
+og_filename=FDTR_CALIBRATION_GibbsExcess
 
-#x0_vals_num=("-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
-#freq_vals_num=("2e6" "4e6" "6e6" "8e6" "10e6")
+#x0_vals_num=("-30" "-15" "-10" "-9" "-8" "-7" "-6" "-5" "-4" "-3" "-2" "-1" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "15" "-30")
+# freq_vals_num=("1e6" "2e6" "4e6" "6e6" "8e6" "10e6")
 
 x0_vals_num=("0")
-freq_vals_num=("1e6")
+freq_vals_num=("1e6" "2e6" "4e6" "6e6" "8e6" "10e6")
 
 # Output file
 output_file="../${og_filename}_out_theta_${theta_angle}.csv"
