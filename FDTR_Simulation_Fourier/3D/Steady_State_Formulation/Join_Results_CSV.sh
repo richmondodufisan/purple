08c38457a35f16446dc4f759dbc4b5260a0c67d1
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-04-01 17:31:55 -0500

    updated joinresults for this particular sweep

--- FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Join_Results_CSV.sh
+++ FDTR_Simulation_Fourier/3D/Steady_State_Formulation/Join_Results_CSV.sh
@@ -11,7 +11,7 @@ og_filename=FDTR_input_Traditional
 # freq_vals_num=("1e6" "2e6" "4e6" "6e6" "8e6" "10e6")
 
 x0_vals_num=("0")
-freq_vals_num=("1e6" "2e6" "4e6" "6e6" "8e6" "10e6")
+freq_vals_num=("1e6" "1.2e6" "1.4e6" "1.6e6" "1.8e6" "2e6" "2.2e6" "2.4e6" "2.6e6" "2.8e6" "3e6" "3.2e6" "3.4e6" "3.6e6" "3.8e6" "4e6" "4.2e6" "4.4e6" "4.6e6" "4.8e6" "5e6" "5.2e6" "5.4e6" "5.6e6" "5.8e6" "6e6" "6.2e6" "6.4e6" "6.6e6" "6.8e6" "7e6" "7.2e6" "7.4e6" "7.6e6" "7.8e6" "8e6" "8.2e6" "8.4e6" "8.6e6" "8.8e6" "9e6" "9.2e6" "9.4e6" "9.6e6" "9.8e6" "10e6")
 
 # Output file
 output_file="../${og_filename}_out_theta_${theta_angle}.csv"
