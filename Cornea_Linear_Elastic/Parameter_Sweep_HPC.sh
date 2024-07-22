49f6f3a4c706c594fe780a45a51ae33da48204bf
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-07-22 15:00:59 -0500

    hpc param sweep bug fix (check for completion first)

--- Cornea_Linear_Elastic/Parameter_Sweep_HPC.sh
+++ Cornea_Linear_Elastic/Parameter_Sweep_HPC.sh
@@ -91,46 +91,53 @@ done
 part2_complete=0
 
 while [ $part2_complete -eq 0 ]; do
+	if check_squeue; then
 
-	for stretch_val_num in "${stretch_vals_num[@]}"; do
-		for freq_val_num in "${freq_vals_num[@]}"; do
+		for stretch_val_num in "${stretch_vals_num[@]}"; do
+			for freq_val_num in "${freq_vals_num[@]}"; do
 
-			# Create a new filename 
-			new_filename_2="${step2_filename}_freq_${freq_val_num}_stretch_${stretch_val_num}.i"
-			mesh_filename_2="${step1_filename}_freq_${freq_val_num}_stretch_${stretch_val_num}_out.e"
-			
-			# Copy the original input file to the new filename
-			cp "$step2_filename$extension2" "$new_filename_2"
-			
-			# Calculate the new length of the plate
-			plate_length=$(python3 -c "import math; print(0.2 / ($freq_val_num / 1e3))")
-			
-			# Replace the plate length in the MOOSE script
-			sed -i "s/\(l_plate\s*=\s*\)[0-9.eE+-]\+/\1$plate_length/g" "$new_filename"
-			
-			# Replace the mesh in the MOOSE script
-			sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$mesh_filename_2\"/" "$new_filename_2"
-			
-			# Replace the frequency in the MOOSE script
-			sed -i "s/\(freq_val\s*=\s*\)[0-9.eE+-]\+/\1$freq_val_num/g" "$new_filename_2"
-			
-			
-			
-			
-			
-			# Replace the input file in the Batch script
-			sed -i "0,/script_name=[^ ]*/s/script_name=[^ ]*/script_name=\"$new_filename_2\"/" "Batch_MOOSE.sh"
-			
-			freq_noexp=$(python3 -c "import math; print(int($freq_val_num*1e-6))")
-			
-			# Replace the job name
-			sed -E -i "s/(#SBATCH --job-name=)[^[:space:]]+/\1${stretch_val_num}_${freq_noexp}_Cornea_Harmonic/" "Batch_MOOSE.sh"
-			
-			# Submit job
-			sbatch Batch_MOOSE.sh
+				# Create a new filename 
+				new_filename_2="${step2_filename}_freq_${freq_val_num}_stretch_${stretch_val_num}.i"
+				mesh_filename_2="${step1_filename}_freq_${freq_val_num}_stretch_${stretch_val_num}_out.e"
+				
+				# Copy the original input file to the new filename
+				cp "$step2_filename$extension2" "$new_filename_2"
+				
+				# Calculate the new length of the plate
+				plate_length=$(python3 -c "import math; print(0.2 / ($freq_val_num / 1e3))")
+				
+				# Replace the plate length in the MOOSE script
+				sed -i "s/\(l_plate\s*=\s*\)[0-9.eE+-]\+/\1$plate_length/g" "$new_filename"
+				
+				# Replace the mesh in the MOOSE script
+				sed -i "0,/file = [^ ]*/s/file = [^ ]*/file = \"$mesh_filename_2\"/" "$new_filename_2"
+				
+				# Replace the frequency in the MOOSE script
+				sed -i "s/\(freq_val\s*=\s*\)[0-9.eE+-]\+/\1$freq_val_num/g" "$new_filename_2"
+				
+				
+				
+				
+				
+				# Replace the input file in the Batch script
+				sed -i "0,/script_name=[^ ]*/s/script_name=[^ ]*/script_name=\"$new_filename_2\"/" "Batch_MOOSE.sh"
+				
+				freq_noexp=$(python3 -c "import math; print(int($freq_val_num*1e-6))")
+				
+				# Replace the job name
+				sed -E -i "s/(#SBATCH --job-name=)[^[:space:]]+/\1${stretch_val_num}_${freq_noexp}_Cornea_Harmonic/" "Batch_MOOSE.sh"
+				
+				# Submit job
+				sbatch Batch_MOOSE.sh
+			done
 		done
-	done
 	
-	part2_complete=1
+		part2_complete=1
+	else
+        echo "Jobs are in the queue. Currently solving iteration no ${submission_count}. Waiting..."
+    fi
+
+    # Sleep for a while before checking again (e.g., 5 minutes)
+    sleep 300
 
 done
