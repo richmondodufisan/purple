511a46906fb0f436339c2c386098934606bf1aef
Alesanmi Richmond Odufisan <vtw1026@quser32.quest.it.northwestern.edu>
2024-07-22 22:32:04 -0500

    last simulations

--- Cornea_Linear_Elastic/Batch_MOOSE.sh
+++ Cornea_Linear_Elastic/Batch_MOOSE.sh
@@ -2,14 +2,14 @@
 #SBATCH --account=p32089  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=10 ## how many cpus or processors do you need on each computer
-#SBATCH --time=0:30:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem-per-cpu=100M ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
-#SBATCH --job-name=1.1_0_Cornea_Stretch  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --ntasks-per-node=20 ## how many cpus or processors do you need on each computer
+#SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=10M ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=1.1_0_Cornea_Harmonic  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --exclude=qnode0565,qnode0626,qnode0637,qnode0019,qnode0115
 
 
-script_name="Cornea_Stretch_freq_10e3_stretch_1.1.i"
+script_name="Cornea_Harmonic_Perturbation_Steady_freq_10e3_stretch_1.1.i"
 
 #moose_exec.sh ../purple-opt -i ${script_name} --mesh-only
 #moose_exec.sh ../purple-opt -i ${script_name}
