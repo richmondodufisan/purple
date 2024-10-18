1cac2f0a847861840f156adfc71b09998abb3d56
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2024-10-17 20:15:16 -0500

    parameter sweep for cornea simulations

---
+++ Cornea_Linear_Elastic/Parameter_Sweep_HPC_Cornea.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Simulation File
+filename="Cornea_Harmonic_Perturbation_2D"
+extension1=".i"
+
+# Define the range of values you want to loop over
+freq_vals_num=("100e3" "1000e3")
+
+# Calculate number of simulations'
+num_freqs=${#freq_vals_num[@]}
+total_simulations=$((num_freqs - 1))
+
+rm *.txt
+
+for freq_val_num in "${freq_vals_num[@]}"; do
+		# Create a new filename 
+		new_filename="${filename}_freq_${freq_val_num}.i"
+
+		# Copy the original input file to the new filename
+		cp "$filename$extension1" "$new_filename"
+		
+		# Replace the stretch ratio in the MOOSE script
+		sed -i "s/\(freq_val\s*=\s*\)[0-9.eE+-]\+/\1$freq_val_num/g" "$new_filename"
+		
+		# Save the new MOOSE script to the list of files
+		echo $new_filename >> Cornea_Simulations.txt
+done
+
+# Update the batch job file with the calculated number of simulations
+sed -i "s/^#SBATCH --array=0-[0-9]\+/#SBATCH --array=0-$total_simulations/" Batch_MOOSE.sh
+
+# Update the simulation list
+sed -i "s|^simulation_list=.*|simulation_list=\"Cornea_Simulations.txt\"|" Batch_MOOSE.sh
+
+# sbatch Batch_MOOSE.sh
---
+++ Cornea_Linear_Elastic/cleanfiles.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm *.out Part1_Stretch_*.i Part2_Harmonic_*.i *.txt *.e *.csv core* *Zone.Identifier*
