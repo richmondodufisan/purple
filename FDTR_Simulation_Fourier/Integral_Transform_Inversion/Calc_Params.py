829d460b6369460f8040c52964018aaecdccafc0
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-01-26 01:14:53 -0600

    re-implemented concentric gaussian in more suggestive form

--- FDTR_Simulation_Fourier/Integral_Transform_Inversion/Calc_Params.py
+++ FDTR_Simulation_Fourier/Integral_Transform_Inversion/Calc_Params.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from Layered_Heat_Conduction import calc_thermal_response
+from Layered_Heat_Conduction_Test_B import calc_thermal_response
 from scipy.optimize import curve_fit
 from scipy.integrate import trapz
 import pdb
@@ -12,39 +12,39 @@ import math
 # File for calculating phase and amplitude for any parameters (for checking)
 # Mostly for debugging purposes
 
-freq = 10 # in MHz
-kappa = 1.8 # in W/(m.K)
+freq = 1 # in MHz
+kappa = 130 # in W/(m.K)
 # conductance = 3e7 # in W/(m2.K)
-conductance = 13e6 # in W/(m2.K)
+conductance = 30e6 # in W/(m2.K)
 
 ################################## CALCULATE PHASE AND AMPLITUDE FOR A SINGLE FREQUENCY #####################################
 
 # Define other parameters required by calc_thermal_response function
-# N_layers = 2
-# layer2 = [40e-6, kappa, kappa, 2329, 689.1]
-# layer1 = [9e-8, 215, 215, 19300, 128.7]
-# layer_props = np.array([layer2, layer1])
-# interface_props = [conductance]
-# r_probe = 1.34e-6
-# r_pump = 1.53e-6
-# pump_power = 0.01
-# calib_consts = [1, 1] # no calibration
-# freq = freq * 1e6
-
-
 N_layers = 2
-layer2 = [100e-6, kappa, kappa, 6180, 249.06]
-layer1 = [80e-9, 215, 215, 19300, 128.5]
+layer2 = [40e-6, kappa, kappa, 2329, 689.1]
+layer1 = [9e-8, 215, 215, 19300, 128.7]
 layer_props = np.array([layer2, layer1])
 interface_props = [conductance]
-r_probe = 1.46e-6
-r_pump = 1.987e-6
-pump_power = 1.5
+w_probe = 1.34e-6
+w_pump = 1.53e-6
+pump_power = 0.01
 calib_consts = [1, 1] # no calibration
 freq = freq * 1e6
 
+
+# N_layers = 2
+# layer2 = [100e-6, kappa, kappa, 6180, 249.06]
+# layer1 = [80e-9, 215, 215, 19300, 128.5]
+# layer_props = np.array([layer2, layer1])
+# interface_props = [conductance]
+# w_probe = 1.46e-6
+# w_pump = 1.987e-6
+# pump_power = 1.5
+# calib_consts = [1, 1] # no calibration
+# freq = freq * 1e6
+
 # Calculate analytical phase 
-phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, r_pump, r_probe, calib_consts, freq, pump_power)
+phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, w_pump, w_probe, calib_consts, freq, pump_power)
 
 print("----------------------------------------------------------------------------------------------")
 print("Phase: " + str(phase) + " radians") 
@@ -107,14 +107,14 @@ print("-------------------------------------------------------------------------
 # layer1 = [40e6 + 90e9, 215, 215, 19300, 128.7]
 # layer_props = np.array([layer1])
 # interface_props = []
-# r_probe = 1.34e-6
-# r_pump = 1.53e-6
+# w_probe = 1.34e-6
+# w_pump = 1.53e-6
 # pump_power = 0.01
 # calib_consts = [1, 1] # no calibration
 # freq = freq * 1e6
 
 # # Calculate analytical phase 
-# phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, r_pump, r_probe, calib_consts, freq, pump_power)
+# phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, w_pump, w_probe, calib_consts, freq, pump_power)
 
 # print("----------------------------------------------------------------------------------------------")
 # print("Phase: " + str(phase)) 
--- FDTR_Simulation_Fourier/Integral_Transform_Inversion/Layered_Heat_Conduction_ConcentricGaussian.py
+++ FDTR_Simulation_Fourier/Integral_Transform_Inversion/Layered_Heat_Conduction_ConcentricGaussian.py
@@ -4,19 +4,25 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.integrate import quad
 from scipy.integrate import quad_vec
+from scipy.special import i0,j0 
 import math
 import cmath
 import pdb
-from scipy.integrate import dblquad
-from scipy.special import i0,j0 
 
 # This is a helper file for calculating phase and temperature increase in FDTR experiments
 # It is assumed that thermal transport is within the diffusive regime, hence Fourier's equation applies
-# The calc_thermal_response should be used to find the phase and amplitude given the appropriate material properties
+# This is the analytical solution for radial heat transfer as detailed in the thesis of AJ Schmidt (2008)
+# The calc_thermal_response function should be used to find the phase and amplitude given the appropriate material properties
+
 
 
 # To use in another file, include the line: 
-# "from Layered_Heat_Conduction_Beam_Offset import calc_thermal_response"
+# "from Layered_Heat_Conduction_ConcentricGaussian import calc_thermal_response"
+
+# This is the same as Layered_Heat_Conduction
+# However, the equations are formatted differently so as to be more explanatory
+# This file serves as a base for potential modifications of the pump/probe gaussians
+
 
 
 # Input arguments for calc_thermal_response:
@@ -25,45 +31,58 @@ from scipy.special import i0,j0
 
 # layer_props: This should be a NumPy array of arrays. The arrays should be organized in descending order,
 # i.e, from the bottom to the top (e.g [layer3, layer2, layer1]). Each layer array should contain the following material
-# properties: [h (layer thickness), kappa_z (cross plane thermal conductivity), kappa_r (in plane/radial thermal conductivity),
+# properties IN ORDER: [h (layer thickness), kappa_z (cross plane thermal conductivity), kappa_r (in plane/radial thermal conductivity),
 # rho (density), c (heat capacity)]
 
 # interface_props: this is an array of the interface conductances between layers. There should be one less
 # conductance than the number of layers (e.g 1 interface conductance for a 2-layer system)
 
-# r_pump: radius of the pump laser
+# w_pump: radius of the pump laser
 
-# r_probe: radius of the probe laser
-
-# x0 (UNIQUE TO BEAM OFFSET): This is a modification of the model to account for beam offsets as detailed in Feser 2012
-# It was found that numerical integration of the approximation (ring profile) was faster than the analytical solution
-# Furthermore, the Hankel transform is defined differently in Feser 2012 (i.e * J(2*pi*k*r)) as opposed to (* J(k*r))
-# in Schmidt 2008
+# w_probe: radius of the probe laser
 
 # calib_constants: additional constants to calibrate model if comparing to FEM simulations with poor/coarse meshes. 
 # The calibration is done on the pump and probe radii.
-# If using experimental data, set each value to 1 (i.e calib_constants = [1, 1])
+# If using experimental data or data from an appropriately refined FEM mesh, set each value to 1 (i.e calib_constants = [1, 1])
 
 # freq: frequency of the input signal (pump laser)
 
-# pump_power: the pump power, Q0
+# pump_power: the pump power, Q0 * absorbance
 
 
-# Define the integrand with respect to r (i.e S(k) = Hankel Transform of S(r))
-# Slightly adjusted version of equation 12 in Feser 2012 to provide
-# a normalized ring-shaped profile, to keep consistent with 
-# Schmidt 2008
 
 
-def integrand_r(r, x0, r_probe, k):
-    exponent_term = np.exp(-2 * (r**2 + x0**2) / r_probe**2)
-    bessel_i_term = i0(4 * x0 * r / r_probe**2)
-    bessel_j_term = j0(k * r)
-    return (4 / r_probe**2) * exponent_term * bessel_i_term * bessel_j_term * r
+
+# Expression for normalized gaussian based on 1/e^2 beam waist
+def pump_integrand_to_hankel(r, w, k):
+    
+    exponent_term = np.exp(  (-2 * (r**2))   /   (w**2)   )
+  
+    prefactor = 2 / (np.pi * (w**2))
+    
+    # Pump gaussian in r
+    P_r = prefactor * exponent_term
+    
+    return P_r * r * j0(k * r)
+    
+    
+    
+def probe_integrand_to_hankel(r, w, k):
+    
+    exponent_term = np.exp(  (-2 * (r**2))   /   (w**2)   )
+  
+    prefactor = 2 / (np.pi * (w**2))
+    
+    # Pump gaussian in r
+    S_r = prefactor * exponent_term
+    
+    return S_r * r * j0(k * r)
 
 
-# Integral in Equation 3.5
-def integrand_k(k, N_layers, layer_props, interface_props, r_pump, r_probe, x0, calib_consts, freq):
+
+
+
+def integrand(k, N_layers, layer_props, interface_props, w_pump, w_probe, calib_consts, freq):
 
   # Checks to ensure data is properly submitted/formatted
   
@@ -72,7 +91,20 @@ def integrand_k(k, N_layers, layer_props, interface_props, r_pump, r_probe, x0, 
     
   if (len(interface_props) != (N_layers - 1)):
     raise RuntimeError("Incorrect number of interface properties")
+    
 
+
+  # Bounds for numerical integral, large number
+  # Contributions beyond 8x the pump/probe radius are negligible
+  r_bound = 50*w_pump
+  
+  # Pump Gaussian in Hankel Space 
+  P_k, _ = quad(pump_integrand_to_hankel, 0, r_bound, args=(w_pump, k))
+  S_k, _ = quad(probe_integrand_to_hankel, 0, r_bound, args=(w_probe, k))
+  
+
+
+  # Calculate Thermal Response Matrix
   ConductionMatrix = np.array([[1, 0], [0, 1]])
   
   for i in range(N_layers):
@@ -108,38 +140,31 @@ def integrand_k(k, N_layers, layer_props, interface_props, r_pump, r_probe, x0, 
   C_total = ConductionMatrix[1][0]
   D_total = ConductionMatrix[1][1]
   
-  # converting from 1/e beam waist to 1/e^2 beam waist
-  r_pump = r_pump * np.sqrt(2)
-  r_probe = r_probe * np.sqrt(2)
-
-  # calibration constants to account for mesh sensitivity  
-  # Only use calibration in extreme cases where a fine mesh is not an option
-  # For most applications, ignore this
-
-  beta1 = calib_consts[0]
-  r_pump = r_pump*beta1
-
-  beta2 = calib_consts[1]
-  r_pump = r_pump*beta2
+  G_k = (-D_total/C_total)
   
-  # G(k) * P(k)
-  G_k_P_k = (-D_total/C_total) * np.exp((-k**2 * r_pump**2)/8)
+
+  return S_k * G_k * P_k * k * 2 * np.pi
   
-  r_bound = 50*r_probe
-  S_k, _ = quad(integrand_r, 0, r_bound, args=(x0, r_probe, k))
-  
-  # G(k) is solution to 3D heat equation in frequency domain + hankel space
-  # P(k) is applied harmonic heat source
-  # S(k) is normalized probe
-  
-  return  S_k * G_k_P_k * k
 
 
-def calc_thermal_response(N_layers, layer_props, interface_props, r_pump, r_probe, x0, calib_consts, freq, pump_power):
-  result, error = quad_vec(integrand_k, 0, 10000001, args=(N_layers, layer_props, interface_props, r_pump, r_probe, x0, calib_consts, freq))
+
+
+def calc_thermal_response(N_layers, layer_props, interface_props, w_pump, w_probe, calib_consts, freq, pump_power):
+
+
+  # Converting from 1/e beam waist to 1/e^2 beam waist
+  # Comment out if using 1/e beam waist
+  w_pump = w_pump * np.sqrt(2)
+  w_probe = w_probe * np.sqrt(2)
+
+
+
+  result, error = quad_vec(integrand, 0, 10000001, args=(N_layers, layer_props, interface_props, w_pump, w_probe, calib_consts, freq))
 
   # Thermal response in frequency domain, Equation 3.5
-  H = (pump_power/(2 * np.pi)) * result
+  # H = (pump_power/(2 * np.pi)) * result
+  
+  H = pump_power * result
 
   phase = math.atan(H.imag/H.real)
   amplitude = cmath.sqrt(H.real**2 + H.imag**2).real
