ed99e4b5a496fddbf86651da58cd48cf5bc7b60c
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-21 12:36:23 -0600

    registered object

--- src/kernels/TotalLagrangianStressDivergenceOriginal.C
+++ src/kernels/TotalLagrangianStressDivergenceOriginal.C
@@ -1,5 +1,7 @@
 #include "TotalLagrangianStressDivergenceOriginal.h"
 
+registerMooseObject("purpleApp", TotalLagrangianStressDivergenceOriginal);
+
 InputParameters
 TotalLagrangianStressDivergenceOriginal::validParams()
 {
