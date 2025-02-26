138c48e525a9a2ac4e58d2931d7a1a2c8b726575
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-25 18:31:39 -0600

    brute force error analysis

--- FDTR_Simulation_Fourier/Integral_Transform_Inversion/Brute_Force_Error_Surface.py
+++ FDTR_Simulation_Fourier/Integral_Transform_Inversion/Brute_Force_Error_Surface.py
@@ -1,24 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from Layered_Heat_Conduction import calc_thermal_response
+
+# from Layered_Heat_Conduction_BesselRing import calc_thermal_response
+from Layered_Heat_Conduction_ConcentricGaussian import calc_thermal_response
+
+
+
+
+
 
 # List of user-provided phases (radians) and frequencies (MHz)
 
-user_phases = [-0.030922435418477924, -0.04469224976972405, -0.06434316989785854, -0.07999274096021089, -0.0935984425367485, -0.10591753116694679, -0.15752124280493338, -0.24011031676668318, -0.31099419948488793, -0.37500734013731735, -0.4337351836666689, -0.667359893013026, -0.9435898417367274, -1.0932834488774827, -1.1845817942656875, -1.2450125780461545]  # Example user-provided phases in radians
-user_frequencies = [0.1, 0.2, 0.4, 0.6, 0.8, 1, 2, 4, 6, 8, 10, 20, 40, 60, 80, 100]  # Corresponding frequencies in MHz
+# CONCENTRIC BEAM DATA
+user_phases = [-0.10596058228041347, -0.1575436103508173, -0.24010833896857903, -0.3109678805719539, -0.3749534252544176, -0.4336496677733477, -0.6670588326830659, -0.9427065152310182, -1.0917640196738556, -1.1824180944615048, -1.242205644005724]  # User-provided phases in radians
+user_frequencies = [1, 2, 4, 6, 8, 10, 20, 40, 60, 80, 100]  # Corresponding frequencies in MHz
 
-# user_phases = [-0.10591753116694679, -0.15752124280493338, -0.24011031676668318, -0.31099419948488793, -0.37500734013731735, -0.4337351836666689]  # Example user-provided phases in radians
-# user_frequencies = [1, 2, 4, 6, 8, 10]  # Corresponding frequencies in MHz
 
-# user_phases = [-0.10591753116694679]  # Example user-provided phases in radians
-# user_frequencies = [1]  # Corresponding frequencies in MHz
-
+# OFFSET BEAM DATA, 3um
+# user_phases = [-0.23799598070042313, -0.3191473825836547, -0.43240560230778396, -0.5238783521854586, -0.6053949444186811, -0.6799793018202053, -0.9718570417488026, -1.2764150773604046, -1.4004094375266116, -1.454875385132239, -1.4807977410339046]  # User-provided phases in radians
+# user_frequencies = [1, 2, 4, 6, 8, 10, 20, 40, 60, 80, 100]  # Corresponding frequencies in MHz
 
 
 # Correct solution coordinates (example values, replace with your actual values)
-correct_kappa = 130  # Replace with actual value
-correct_conductance = 30e6  # Replace with actual value
+correct_kappa = 130  # Assuming Isotropic
+correct_conductance = 30e6
+
 
 
 
@@ -34,28 +41,47 @@ conductance_values = np.linspace(1e6, 59e6, 100)  # Discretize conductance withi
 # Initialize the cumulative error array
 error_surface = np.zeros((len(kappa_values), len(conductance_values)))
 
+
+
+
+
 # Loop over each phase and frequency
 for user_phase, user_frequency in zip(user_phases, user_frequencies):
+
     # Calculate phase at each grid point and compute the error for this frequency/phase pair
     for i, kappa in enumerate(kappa_values):
+    
+    
         for j, conductance in enumerate(conductance_values):
-            # Define other parameters required by calc_thermal_response function
+        
+            # Define parameters required by calc_thermal_response function
+            
             N_layers = 2
             layer2 = [40e-6, kappa, kappa, 2329, 689.1]
-            layer1 = [90e-9, 215, 215, 19300, 128.7]
+            layer1 = [9e-8, 215, 215, 19300, 128.7]
             layer_props = np.array([layer2, layer1])
             interface_props = [conductance]
-            r_probe = 1.34e-6
-            r_pump = 1.53e-6
+            w_probe = 1.34e-6
+            w_pump = 1.53e-6
             pump_power = 0.01
-            calib_consts = [1, 1]  # No calibration
-            freq = user_frequency * 1e6  # Convert MHz to Hz
+            offset = 3e-6
+            freq = user_frequency * 1e6
+            
 
             # Calculate the phase using the provided model
-            phase, _ = calc_thermal_response(N_layers, layer_props, interface_props, r_pump, r_probe, calib_consts, freq, pump_power)
+            phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, w_pump, w_probe, freq, pump_power)
+            
+            # phase, amplitude = calc_thermal_response(N_layers, layer_props, interface_props, w_pump, w_probe, offset, freq, pump_power)
+
+
 
             # Compute the error (squared difference between the calculated and user-provided phase)
             error_surface[i, j] += (phase - user_phase) ** 2  # Accumulate the error for this phase/frequency pair
+
+
+
+
+
 
 # Plot the cumulative error surface as a 2D contour plot
 kappa_grid, conductance_grid = np.meshgrid(kappa_values, conductance_values)
@@ -74,7 +100,8 @@ plt.scatter(correct_kappa, correct_conductance / 1e6, color='red', marker='x', s
 plt.legend()
 
 
-plt.savefig('cumulative_error_plot_2D_18_freq.png')
+plt.savefig('cumulative_error_plot_2D_Concentric.png')
+# plt.savefig('cumulative_error_plot_2D_BesselRing_3um.png')
 plt.show()
 
 # # 3D Plot of the cumulative error surface
