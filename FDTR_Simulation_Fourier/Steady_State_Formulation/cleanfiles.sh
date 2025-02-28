6a4a36b7cf2af88e02588808899309b064830267
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-02-27 19:45:26 -0600

    simulation settings

--- FDTR_Simulation_Fourier/Steady_State_Formulation/cleanfiles.sh
+++ FDTR_Simulation_Fourier/Steady_State_Formulation/cleanfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm *Zone* *.out FDTR_mesh_x0_* FDTR_input_GibbsExcess_StepFunction_BesselRing_*.i DTR_input_GibbsExcess_StepFunction_Concentric_*.i FDTR_input_GibbsExcess_Interface_Fourier_Steady_*.i FDTR_input_Traditional_Fourier_Steady_*.i FDTR_CALIBRATION_Fourier_Steady_*.i *.txt *.e *.csv core*
+rm *Zone* *.out FDTR_mesh_x0_*py FDTR_input_GibbsExcess_StepFunction_BesselRing_*.i DTR_input_GibbsExcess_StepFunction_Concentric_*.i FDTR_input_GibbsExcess_Interface_Fourier_Steady_*.i FDTR_input_Traditional_Fourier_Steady_*.i FDTR_CALIBRATION_Fourier_Steady_*.i *.txt *.e *.csv core*
