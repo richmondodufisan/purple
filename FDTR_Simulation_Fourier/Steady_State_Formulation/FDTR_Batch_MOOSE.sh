1fef497f20ddfb0b3c7ca0669a8d5f40381f771b
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2025-03-03 13:51:19 -0600

    updated FDTR Batch script

--- FDTR_Simulation_Fourier/Steady_State_Formulation/FDTR_Batch_MOOSE.sh
+++ FDTR_Simulation_Fourier/Steady_State_Formulation/FDTR_Batch_MOOSE.sh
@@ -2,15 +2,13 @@
 #SBATCH --account=p32089  ## YOUR ACCOUNT pXXXX or bXXXX
 #SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --array=0-428
-#SBATCH --nodes=1
+#SBATCH --nodes=5
 #SBATCH --ntasks-per-node=20 ## how many cpus or processors do you need on each computer
 #SBATCH --time=4:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=4G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=fourier  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --exclude=qnode0565
 
-#moose_exec.sh ../purple-opt -i ${script_name} --mesh-only
-#moose_exec.sh ../purple-opt -i ${script_name}
 
 module purge
 module load mamba/24.3.0
@@ -18,6 +16,16 @@ module load git
 eval "$(conda shell.bash hook)"
 
 conda activate /projects/p32089/envs/moose
+
+
+# Manually set the PATH to ensure consistency
+export PATH=/projects/p32089/envs/moose/bin:$PATH
+
+# Debug: Print environment variables
+echo "PATH inside job: $PATH"
+echo "CONDA_PREFIX: $CONDA_PREFIX"
+echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+
 
 IFS=$'\n' read -d '' -r -a lines < SteadyStateFourier.txt
 
