81f2b5eb5f49206d34dde436938efee3f31075bf
Richmond <alesanmiodufisan2028@u.northwestern.edu>
2026-01-11 14:32:30 -0600

    latest

--- FDTR_Simulation_Fourier/Axisymmetric_Steady_State_Inversion/cleanfiles.sh
+++ FDTR_Simulation_Fourier/Axisymmetric_Steady_State_Inversion/cleanfiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm *.out FDTR_input_GibbsExcess_StepFunction_Fourier_Steady_*.i FDTR_input_GibbsExcess_Interface_Fourier_Steady_*.i FDTR_input_Traditional_Fourier_Steady_*.i FDTR_CALIBRATION_Fourier_Steady_*.i *.txt *.e *.csv core* *temp*
+rm *.out FDTR_input_GibbsExcess_StepFunction_Fourier_Steady_*.i FDTR_input_Traditional_Axisymmetric_*.i FDTR_input_GibbsExcess_Interface_Fourier_Steady_*.i FDTR_input_Traditional_Fourier_Steady_*.i FDTR_CALIBRATION_Fourier_Steady_*.i *.txt *.e *.csv core* *temp*
